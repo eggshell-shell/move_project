8058215abb227d5a3bb576aac4a29191ac716c46
itheima <itheima@qq.com>
2021-03-08 13:48:56 +0800

    完成验证码获取及身份令牌持久化处理、响应式

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: JSON.parse(window.localStorage.getItem('Token_key'))
   },
   mutations: {
+    setUser(state, data) {
+      // 数据响应式处理
+      state.user = data
+
+      // 防止刷新丢失数据，持久化处理
+      window.localStorage.setItem('Token_key', JSON.stringify(state.user))
+    }
   },
   actions: {
   },
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="login-container">
     <van-nav-bar class="van-nav-bar" title="登录" />
-    <van-form @submit="onSubmit">
+    <van-form ref="loginFrom" @submit="onSubmit">
       <van-field
         v-model="user.mobile"
-        name="手机号"
+        type="number"
+        maxlength="11"
+        name="mobile"
         placeholder="填写手机号"
-        :rules="[{ required: true, message: '请填写手机号' }]"
+        :rules="userFormRules.mobile"
       >
         <i slot="left-icon" class="iconfont iconshouji"></i>
       </van-field>
       <van-field
         v-model="user.code"
-        name="验证码"
+        name="code"
+        type="number"
+        maxlength="6"
         placeholder="填写验证码"
-        :rules="[{ required: true, message: '请填写验证码' }]"
+        :rules="userFormRules.code"
       >
         <i slot="left-icon" class="iconfont iconyanzhengma"></i>
         <template #button>
-          <van-button class="send_number" size="small" type="default" round
+          <van-button
+            v-if="flag"
+            class="send_number"
+            size="small"
+            type="default"
+            round
+            @click="onSendSms(user.mobile)"
+            native-type="button"
             >发送验证码</van-button
           >
+          <van-count-down
+            v-else
+            millisecond
+            time="60000"
+            format="ss:SS 秒后"
+            @finish="flag = true"
+          />
         </template>
       </van-field>
       <div style="margin: 16px">
@@ -33,30 +51,66 @@
 </template>
 
 <script>
-import { login } from "@/api/user";
+import { login, sendSms } from "@/api/user";
+import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
 
 export default {
   name: "login",
   data() {
     return {
       user: {
-        mobile: "",
-        code: "",
+        mobile: "18940975135",
+        code: "246810",
       },
+      userFormRules: {
+        mobile: [
+          { required: true, message: "请填写手机号" },
+          { pattern: /^[1][3|5|7|8]\d{9}$/, message: "手机号码格式错误" },
+        ],
+        code: [
+          { required: true, message: "请填写验证码" },
+          { pattern: /^\d{6}$/, message: "验证码格式错误" },
+        ],
+      },
+      flag: true,
     };
   },
   methods: {
     async onSubmit() {
+      this.$toast.loading({
+        message: "登录中...",
+        forbidClick: true,
+        // 持续时间
+        duration: 0,
+      });
       const user = this.user;
       try {
-        const res = await login(user);
-        console.log("成功");
+        const { data } = await login(user);
+        this.$toast.success("登陆成功");
+        this.$store.commit("setUser", data.data);
       } catch (err) {
         if (err.response.status === 400) {
-          console.log("验证码错误");
+          this.$toast.fail("验证码错误");
         } else {
-          console.log("服务器错误");
+          this.$toast.fail("服务器出错");
         }
+      }
+    },
+    async onSendSms(mobile) {
+      try {
+        await this.$refs.loginFrom.validate("mobile");
+        this.$toast.success("手机号验证通过");
+        this.flag = false;
+        try {
+          const res = await sendSms(mobile);
+        } catch (err) {
+          console.log(err);
+          if (err.response.status === 429)
+            return this.$toast.fail("过于频繁，稍后尝试");
+        }
+      } catch (err) {
+        this.$toast.fail("手机号验证出错");
+        return;
       }
     },
   },
